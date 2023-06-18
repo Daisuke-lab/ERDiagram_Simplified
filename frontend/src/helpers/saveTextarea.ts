a0e7591bd0f62caba09780af772afc193fc43a33
daisuke <daisuke.zr@icloud.com>
2023-06-18 16:11:37 +0900

    debug code added

--- frontend/src/helpers/saveTextarea.ts
+++ frontend/src/helpers/saveTextarea.ts
@@ -22,6 +22,9 @@ export default function saveTextarea(state:RootState, dispatch:AppDispatch, stag
 
     let newShape = null
     console.log("you are in saveTextarea")
+    console.log(textarea)
+    console.log(session)
+    console.log(stage)
     if (textarea !== null && session !== null && stage !== null && stage !== undefined) {
         console.log("you found textarea")
         const canvasText:Konva.default.Text = stage.findOne(`#${textarea.getAttribute("id")}`)
