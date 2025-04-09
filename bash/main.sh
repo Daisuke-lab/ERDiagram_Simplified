aa50f2baaba58344021a34d56ed6beea4d0d1db5
Daisuke-lab <kikuchidaisuke.zr@gmail.com>
2025-04-08 21:57:00 -0500

    boostup shell modified

--- bash/main.sh
+++ bash/main.sh
@@ -7,13 +7,6 @@ echo 'alias kubectl="sudo k3s kubectl"' >> $HOME/.bashrc
 source $HOME/.bashrc
 
 
-## Allocate IP
-INSTANCE_ID=$(ec2-metadata -i | awk '{print $2}')
-ELASTIC_IP_ALLOCATION_ID=$(aws ec2 describe-addresses --query "Addresses[*].AllocationId" --output text)
-aws ec2 associate-address --instance-id $INSTANCE_ID --allocation-id $ELASTIC_IP_ALLOCATION_ID --allow-reassociation
-
-
-
 ## Pull config files
 aws s3 cp s3://er-diagram-config/application.properties .
 aws s3 cp s3://er-diagram-config/.env.local .
@@ -23,3 +16,9 @@ kubectl create configmap er-frontend-config --from-file=.env.local
 ## Apply templates
 export KS_DIR="$HOME/ERDiagram_Simplified/kubernetes"
 kubectl apply -f $KS_DIR --force
+
+
+## Allocate IP
+INSTANCE_ID=$(ec2-metadata -i | awk '{print $2}')
+ELASTIC_IP_ALLOCATION_ID=$(aws ec2 describe-addresses --query "Addresses[*].AllocationId" --output text)
+aws ec2 associate-address --instance-id $INSTANCE_ID --allocation-id $ELASTIC_IP_ALLOCATION_ID --allow-reassociation
